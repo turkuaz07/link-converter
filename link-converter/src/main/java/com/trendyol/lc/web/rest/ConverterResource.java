d4760f969ede202f304e8dbf78b80577ae576ace
Ali Çevik <cvkali@gmail.com>
2021-12-05 19:14:55 +0300

    Task 1

---
+++ link-converter/src/main/java/com/trendyol/lc/web/rest/ConverterResource.java
@@ -0,0 +1,11 @@
+package com.trendyol.lc.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/conversion")
+public class ConverterResource {
+
+	
+}
